8a773c05bb663574e51b54bd0892b31f2f132632
praiseee <S10255495@connect.np.edu.sg>
2024-08-08 15:42:57 +0800

    Grandfather AI state Draft 1

---
+++ Assets/Scripts/GrandfatherAI.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GrandfatherAI : MonoBehaviour
+{
+    /// <summary>
+    /// Assign the WalkPoint GameObject in Inspector
+    /// </summary>
+    public GameObject walkPoint;
+
+    /// <summary>
+    /// Assign the Animator component in Inspector
+    /// </summary>
+    public Animator animator;
+
+    /// <summary>
+    /// Assign the NavMeshAgent in component in the Inspector
+    /// </summary>
+    public NavMeshAgent agent; 
+
+    private bool shouldMove = false;
+
+    void Update()
+    {
+        if (shouldMove)
+        {
+            MoveToWalkPoint();
+        }
+    }
+
+    void MoveToWalkPoint()
+    {
+        agent.SetDestination(walkPoint.transform.position);
+        animator.SetBool("isWalking", true);
+
+        // Stop moving after reaching the destination
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            animator.SetBool("isWalking", false);
+            shouldMove = false;
+        }
+    }
+
+    public void StartMoving()
+    {
+        shouldMove = true;
+    }
+}
